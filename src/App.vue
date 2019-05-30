08fe10c3ac27f7508b49aad82b893be7f02b0c31
yiy142 <yiy142@ucsd.edu>
2019-05-30 13:56:36 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to jj-components on Vue.js!"/>
   </div>
 </template>
 
